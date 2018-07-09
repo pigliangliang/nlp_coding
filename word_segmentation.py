fc01ce6731f2dbbfff0417e17fddb78dd896d57b
zhuxiaoliang <836904717@qq.com>
2018-07-09 18:37:35 +0800

    新增hmm算法分词

--- word_segmentation.py
+++ word_segmentation.py
@@ -69,5 +69,160 @@ if __name__ == '__main__':
 1）建立统计语言模型
 2）对句子进行单词划分，然后对分词结果进行概率计算，获得概率最大的分词结果。比如：HMM 和CRF
 
+语言模型：
+用概率论的专业术解释语言模型就是：为长度为m的字符串确定其概率分布。p(w1,w2.....wm),其中w1...wm依次表示文本中的各个词语。一般用链式法则计
+算概率值 p(w1,w2....)= P(w1)P(w2|w1)P(w3|w1,w2)....P(wm|w1,w2....wm-1)
+上式当文本过长，计算难度越来越大。
+n——gram模型：
+n=1:p(w1,w2....)=p(w1)p(w2)...p(wm)
+n=2:p(w1,w2....)=p(w1)p(w2|pw1)...p(wi|wi-1)
+n=2:p(w1,w2....)=p(w1)p(w2|pw1)...p(wi|wi-1,wi-2)
+
+HMM模型：
+隐马尔科夫模型将分词作为字在字符串中序列标注任务来实现的。基本思路是：每个字在构造一个特定的词语时都占据着一个确定的构词位置，规定每个字最多有
+四个构词位置：B词首，M词中，E词尾，S单独成词。
+用数学抽象表示：
+X=X1X2...Xn代表输入的句子，n表示句子的长度，Xn表示句中的字，Y=Y1Y2...Yn，为BMSE四中标记
+这里引入观测独立性行假设：即每个字段输出仅仅与当前字有关
+于是能得到公式：p(y1y2...yn|x1x2...xn) = p(y1|x1).....p(yn|xn)
+基于观测独立性假设，完全没有考虑上下文，计算容易，但是会出现不合理情况。
+引入HMM解决这个问题。
+上式中解决p（y|x）问题，根据贝叶斯公式，p（Y|X）= p(x|y)p(y)/p（x）
+对p（x|y）做马尔科夫驾驶
+p（x|y）=p(x1|y1)p(x2|y2)...p(xn|yn)
+对p(y)引入马尔科夫另一假设，每个输出仅仅个上一个输出有关系。
+p（y）= p(y1)p(y2|y1)...p(yn|yn-1)
+求解HMM的过程中使用了vetervi算法：核心思想是如果最终的最优路径经过某个xi，那么从最初节点到xi-1节点的路径也是最优路径。
+python 实现HMM
+
 
 '''
+class HMM(object):
+    def __init__(self):
+        import os
+        self.model_file = 'hmm_model.pkl'
+        self.state_list = ['B','M','E','S']
+        self.load_para = False
+    def try_load_model(self,trained):
+        if trained:
+            import pickle
+            with open(self.model_file,'rb') as f :
+                self.A_dic = pickle.load(f)
+                self.B_dic = pickle.load(f)
+                self.Pi_dic = pickle.load(f)
+                self.load_para = True
+        else:
+            #转移概率(状态到状态条件概率)
+            self.A_dic = {}
+            #发射概率(状态到词语概率)
+            self.B_dic = {}
+            #状态的初始概率
+            self.Pi_dic = {}
+            self.load_para = False
+
+    def train(self,path):
+        #充值概率矩阵
+        self.try_load_model(False)
+        #统计状态出现次数
+        Count_dic = {}
+        #初始化参数
+        def init_parameters():
+            for state in self.state_list:
+                self.A_dic[state]={s:0.0 for s in self.state_list}
+                self.Pi_dic[state]=0.0
+                self.B_dic[state]={}
+                Count_dic[state]=0
+        def maklabel(text):
+            cut_text = []
+            if len(text) == 1:
+                cut_text.append("S")
+            else:
+                cut_text += ['B'] +['M']*(len(text)-2)+['E']
+            return cut_text
+        init_parameters()
+        line_num = -1
+        words = set()
+        with open(path,encoding="utf-8") as f:
+            for line in f:
+                line_num +=1
+                if not  line:
+                    continue
+                word_list = [i for i in line if i !=' ' and i !='\n']
+                words |=set(word_list)
+                linelist = line.split()
+                line_state = []
+                for w in linelist:
+                    line_state.extend(maklabel(w))
+                assert len(word_list)==len(line_state)
+                for k,v in enumerate(line_state):
+                    Count_dic[v] += 1
+                    if k == 0:
+                        #每个句子的第一个字的处状态，用于计算初始状态概率
+                        self.Pi_dic[v] +=1
+                    else:
+                        self.A_dic[line_state[k-1]][v] +=1
+                        self.B_dic[line_state[k]][word_list[k]] = \
+                            self.B_dic[line_state[k]].get(word_list[k],0) +1.0
+        self.Pi_dic = {k:v*1.0 / line_num for k,v in self.Pi_dic.items()}
+        self.A_dic = {k:{k1:v1/Count_dic[k] for k1 ,v1 in v.items()} for k,v in self.A_dic.items()}
+        self.B_dic = {k: {k1: (v1 + 1) / Count_dic[k] for k1, v1 in v.items()} for k,v in self.B_dic.items()}
+        import pickle
+        with open(self.model_file,'wb') as f:
+            pickle.dump(self.A_dic,f)
+            pickle.dump(self.B_dic,f)
+            pickle.dump(self.Pi_dic,f)
+        return self
+
+    def veterbi(self,text,states,start_p,trans_p,emit_p):
+        V = [{}]
+        path = {}
+        for y in  states:
+            V[0][y] = start_p[y]*emit_p[y].get(text[0],0)
+            path[y] =[y]
+        for t in range(1,len(text)):
+            V.append({})
+            newpath = {}
+
+            neverSeen = text[t] not in emit_p['S'].keys() and \
+            text[t] not in emit_p['M'].keys() and \
+            text[t] not in emit_p['E'].keys() and \
+            text[t] not in emit_p['B'].keys()
+
+            for y in states:
+                emitP =emit_p[y].get(text[t],0) if not  neverSeen else 1.0
+
+                (prob,state) = max([(V[t-1][y0]*trans_p[y0].get(y,0) *emitP,y0) for y0 in states if V[t-1][y0]>0])
+                V[t][y] = prob
+                newpath[y] = path[state]+[y]
+            path = newpath
+
+        if emit_p['M'].get(text[-1],0)>emit_p['S'].get(text[-1],0):
+            (prob,state)=max([(V[len(text)-1][y],y) for y in ('E','M')])
+        else:
+            (prob, state) = max([(V[len(text) - 1][y], y) for y in states])
+        return (prob,path[state])
+    def cut(self,text):
+        import os
+        if not self.load_para:
+            self.try_load_model(os.path.exists(self.model_file))
+        prob,pos_list = self.veterbi(text,self.state_list,self.Pi_dic,self.A_dic,self.B_dic)
+        begin,next = 0,0
+        for i ,char in enumerate(text):
+            pos = pos_list[i]
+            if pos == 'B':
+                begin = i
+            elif pos == 'E':
+                yield text[begin:i+1]
+            elif pos == 'S':
+                yield char
+                next = i +1
+        if next <len(text):
+            yield text[next:]
+
+hmm = HMM()
+hmm.train('hmm')
+for h in hmm.cut('在这个美丽的夜晚我睡着了'):
+    print(h,end= ' ')
+"""
+在 这个 美丽 的 夜晚 我睡 着 了 
+"""
